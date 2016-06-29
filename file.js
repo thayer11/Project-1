45ab2457efd5309041872a7b28bb65d7413c5bda
thayer11 <holly.coddington@gmail.com>
2016-06-28 23:53:38 -0600

    jsPsuedo-code

---
+++ file.js
@@ -0,0 +1,45 @@
+/*
+
+Function Load Page 
+
+Constructor:
+Objects=EachCard which exists in an array
+properties:
+sideOne: image
+sideTwo: color
+sideNothing:" "
+
+event listener on "Place Cards" button in HTML to trigger array referrring to Dom locations 
+gamesquare on gameboard 
+randomly math.random?
+
+event listener on Start Time var start=3 
+if time=0
+message "Sorry, you are probably great at other things" and run reset function
+else time runs
+
+each dom gamesquare event listener listens for click which triggers function turnOver
+turn over changes property of card object from sideOne to sideTwo 
+
+for loop to iterate through objects in array
+if EachCard sideTwo [0] || EachCard sideTwo[1]!= EachCard sideTwo[2] 
+then EachCard will go back to sideOne
+else EachCard sideTwo[1]===sideTwo[2]
+
+When EachCard in Array has a sideTwo == to EachCard sideTwo (when the array has been reduced to 8)
+game over 
+
+event listener in Reset if clicked property of card will be " ".
+
+
+
+
+
+
+
+
+
+
+
+
+*/
