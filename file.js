7dc2b67169536713ba558282c3637109408fef1e
thayer11 <holly.coddington@gmail.com>
2016-06-29 12:48:26 -0600

    tileImages

--- file.js
+++ file.js
@@ -1,35 +1,58 @@
-/*
+window.onload=function() {
+ 
+ function Card(side1, side2) {
+    this.side1 = side1;
+    this.side2 = side2;
+ }
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+ var card1 = new card("ivory","image");
+//Function Load Page 
 
-Function Load Page 
+//Constructor:
+//Objects=EachCard which exists in an array
+//properties:
+//sideOne: image
+//sideTwo: color
+//sideNothing:" "
 
-Constructor:
-Objects=EachCard which exists in an array
-properties:
-sideOne: image
-sideTwo: color
-sideNothing:" "
 
-event listener on "Place Cards" button in HTML to trigger array referrring to Dom locations 
-gamesquare on gameboard 
-randomly math.random?
+//event listener on "Place Cards" button in HTML to trigger array referrring to Dom locations 
+//gamesquare on gameboard 
+//randomly math.random?
 
-event listener on Start Time var start=3 
-if time=0
-message "Sorry, you are probably great at other things" and run reset function
-else time runs
+//event listener on Start Time var start=3 
+//if time=0
+//message "Sorry, you are probably great at other things" and run reset function
+//else time runs
 
-each dom gamesquare event listener listens for click which triggers function turnOver
-turn over changes property of card object from sideOne to sideTwo 
+//each dom gamesquare event listener listens for click which triggers function turnOver
+//turn over changes property of card object from sideOne to sideTwo 
 
-for loop to iterate through objects in array
-if EachCard sideTwo [0] || EachCard sideTwo[1]!= EachCard sideTwo[2] 
-then EachCard will go back to sideOne
-else EachCard sideTwo[1]===sideTwo[2]
+//for loop to iterate through objects in array
+//if EachCard sideTwo [0] || EachCard sideTwo[1]!= EachCard sideTwo[2] 
+//then EachCard will go back to sideOne
+//else EachCard sideTwo[1]===sideTwo[2]
 
-When EachCard in Array has a sideTwo == to EachCard sideTwo (when the array has been reduced to 8)
-game over 
+//When EachCard in Array has a sideTwo == to EachCard sideTwo (when the array has been reduced to 8)
+//game over 
 
-event listener in Reset if clicked property of card will be " ".
+//event listener in Reset if clicked property of card will be " ".
 
 
 
